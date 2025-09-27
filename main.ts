1e8318571a69dc71a8b8c4286e2cb1d8049204e0
Ignat <ignat@eyelinkmedia.com>
2025-09-27 09:51:33 +0100

    fix some formating

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";// main.ts
+import "$std/dotenv/load.ts"; // main.ts
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
